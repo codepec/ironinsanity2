b4892b11b8a72957a490448d096878a3a0134540
codepec <codepec.de@gmail.com>
2026-04-29 21:41:15 +0200

    Fix script path in index.html, update package.json for gh-pages, and adjust vite.config.ts comments

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ironinsanity2/', // <--- wichtig für GitHub Pages
+  base: '/ironinsanity2/', 
 })
